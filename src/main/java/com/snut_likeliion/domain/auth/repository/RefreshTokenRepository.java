77c3b28fa3fabec51a8bba7f13b10f16fc6eec78
maruhxn <maruhan1016@gmail.com>
2025-05-04 20:09:14 +0900

    feat#1: RefreshToken Entity & Repository 추가

---
+++ src/main/java/com/snut_likeliion/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,16 @@
+package com.snut_likeliion.domain.auth.repository;
+
+
+import com.snut_likeliion.domain.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByPayload(String payload);
+
+    Optional<RefreshToken> findByEmail(String email);
+
+    void deleteAllByEmail(String email);
+}
