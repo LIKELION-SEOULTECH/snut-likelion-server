b58306dcd62cddb0d6f463061fa59335a74fb8fb
maruhxn <maruhan1016@gmail.com>
2025-05-21 18:36:18 +0900

    feat#6: ProjectRetrospectionController & ProjectRetrospectionService 구현

---
+++ src/main/java/com/snut_likeliion/domain/project/controller/ProjectRetrospectionController.java
@@ -0,0 +1,67 @@
+package com.snut_likeliion.domain.project.controller;
+
+import com.snut_likeliion.domain.project.dto.request.CreateRetrospectionRequest;
+import com.snut_likeliion.domain.project.dto.request.UpdateRetrospectionRequest;
+import com.snut_likeliion.domain.project.dto.response.RetrospectionResponse;
+import com.snut_likeliion.domain.project.service.ProjectRetrospectionService;
+import com.snut_likeliion.global.auth.model.SnutLikeLionUser;
+import com.snut_likeliion.global.dto.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/projects/{projectId}/retrospection")
+@RequiredArgsConstructor
+public class ProjectRetrospectionController {
+
+    private final ProjectRetrospectionService projectRetrospectionService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<Object> createProjectRetrospection(
+            @AuthenticationPrincipal SnutLikeLionUser writer,
+            @PathVariable("projectId") Long projectId,
+            @RequestBody @Valid CreateRetrospectionRequest req
+    ) {
+        projectRetrospectionService.create(projectId, writer.getId(), req);
+        return ApiResponse.success("프로젝트 회고 작성 성공");
+    }
+
+    @GetMapping
+    public ApiResponse<List<RetrospectionResponse>> getProjectRetrospections(
+            @PathVariable("projectId") Long projectId
+    ) {
+        return ApiResponse.success(
+                projectRetrospectionService.getAllByProjectId(projectId),
+                "프로젝트 회고 목록 조회 성공"
+        );
+    }
+
+    // TODO: authChecker
+    @PatchMapping("/{retrospectionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void modifyProjectRetrospection(
+//            @AuthenticationPrincipal SnutLikeLionUser writer,
+            @PathVariable("projectId") Long projectId,
+            @PathVariable("retrospectionId") Long retrospectionId,
+            @RequestBody @Valid UpdateRetrospectionRequest req
+    ) {
+        projectRetrospectionService.modify(retrospectionId, req);
+    }
+
+    // TODO: authChecker
+    @DeleteMapping("/{retrospectionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProjectRetrospection(
+//            @AuthenticationPrincipal SnutLikeLionUser writer,
+            @PathVariable("projectId") Long projectId,
+            @PathVariable("retrospectionId") Long retrospectionId
+    ) {
+        projectRetrospectionService.remove(retrospectionId);
+    }
+}
--- src/main/java/com/snut_likeliion/global/auth/PermitAllUrls.java
+++ src/main/java/com/snut_likeliion/global/auth/PermitAllUrls.java
@@ -15,7 +15,10 @@ public enum PermitAllUrls {
     CERTIFY_CODE("/api/v1/auth/email/certify", POST),
     FIND_PASSWORD("/api/v1/auth/password/find", POST),
     CHANGE_PASSWORD("/api/v1/auth/password/change", PATCH),
-    REFRESH("/api/v1/auth/refresh", GET);
+    REFRESH("/api/v1/auth/refresh", GET),
+    GET_ALL_PROJECTS("/api/v1/projects", GET),
+    GET_PROJECT("/api/v1/projects/{projectId}", GET),
+    GET_PROJECT_RETROSPECTIONS("/api/v1/projects/{projectId}/retrospection", GET);
 
     private final String url;
     private final HttpMethod method;
