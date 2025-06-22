c968f560183a79c70f037015c820e4274cd45617
maruhxn <maruhan1016@gmail.com>
2025-06-22 18:01:38 +0900

    fix: 이메일 인증 코드 반환 시 LIMIT 1 추가

--- src/main/java/com/snut_likelion/domain/auth/repository/CertificationTokenRepository.java
+++ src/main/java/com/snut_likelion/domain/auth/repository/CertificationTokenRepository.java
@@ -2,9 +2,12 @@ package com.snut_likelion.domain.auth.repository;
 
 import com.snut_likelion.domain.auth.entity.CertificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CertificationTokenRepository extends JpaRepository<CertificationToken, Long> {
+
+    @Query("SELECT ct FROM CertificationToken ct WHERE ct.email = :email ORDER BY ct.expiredAt DESC LIMIT 1")
     Optional<CertificationToken> findByEmail(String email);
 }
